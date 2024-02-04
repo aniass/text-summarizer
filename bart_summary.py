98424177aebed359aead2c72f20cc3773f351488
Aniass <a.staskiewicz4@gmail.com>
2024-02-04 18:03:29 +0100

    code refactor

--- bart_summary.py
+++ bart_summary.py
@@ -1,5 +1,4 @@
-from transformers import pipeline
-from transformers import BartTokenizer, BartForConditionalGeneration
+from transformers import pipeline, BartTokenizer, BartForConditionalGeneration
 
 PATH = '/data/text1.txt'
 
@@ -14,33 +13,33 @@ def read_data(filepath):
         return text
 
 
-def get_summary(text):
+def transformers_summary(text):
     """Function to get summary by transformers library"""
     summarizer = pipeline("summarization")
     summaries = summarizer(text, min_length=10, max_length=300)
     summary_text = ' '.join([str(i) for i in summaries])
-    print(summary_text)
+    return summary_text
 
 
 def bart_summary(text):
     """Function to get summary by bart model"""
-    input = tokenizer.encode(text, return_tensors="pt", max_length=512)
-    summary_ids = model.generate(input, max_length=160, min_length=12,
+    input_ids = tokenizer.encode(text, return_tensors="pt", max_length=512)
+    summary_ids = model.generate(input_ids, max_length=160, min_length=12,
                                  length_penalty=1.0, num_beams=4,
                                  early_stopping=True)
     output_summ = [tokenizer.decode(g, skip_special_tokens=True,
                                     clean_up_tokenization_spaces=False)
                    for g in summary_ids]
-    print(output_summ)
+    return output_summ
 
 
 def main():
     text = read_data(PATH)
-    print(f'The summary created by transformers library:\n')
-    get_summary(text)
-    print('----------------------------------')
-    print(f'The summary created by bart model:\n')
-    bart_summary(text)
+    if text:
+        print(f'The summary created by transformers library:\n')
+        print(transformers_summary(text))
+        print(f'The summary created by bart model:\n')
+        print(bart_summary(text))
 
 
 if __name__ == '__main__':
